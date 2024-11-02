fbb02596f210d843342c9a03155f9c890f0050e5
Candice Cirbo <thecirbos@icloud.com>
2024-11-02 13:42:52 -0600

    Feat: refactored controller to make skinny for now, controller is not done

--- app/controllers/api/v1/animals_controller.rb
+++ app/controllers/api/v1/animals_controller.rb
@@ -1,4 +1,11 @@
 class Api::V1::AnimalsController < ApplicationController
+
+# def show_home
+# animal = "rabbit"
+
+# animal = Animal.find_by(name: "animal name") || animal.fetch from api
+#  def predator chain
+
   def create
     if params[:action_type] == "start"
       rabbit = Animal.find_by(name: "rabbit")
@@ -6,48 +13,16 @@ class Api::V1::AnimalsController < ApplicationController
       if rabbit
         predators = rabbit.predators.split(',').map(&:strip).first(3)
         predators_data = predators.map do |predator_name|
-          animal_api_key = Rails.application.credentials.ninja_animals[:key]
-          picture_api_key = Rails.application.credentials.pexels[:key]
-          animal_conn = Faraday.new(url: "https://api.api-ninjas.com")
-          picture_conn = Faraday.new(url: "https://api.pexels.com") do |faraday|
-            faraday.headers["Authorization"] = Rails.application.credentials.pexels[:key]
-          end
-        
-          animal_response = animal_conn.get("/v1/animals?name=#{predator_name}", { "X-Api-Key": animal_api_key })
-          photo_response = picture_conn.get("/v1/search?query=#{predator_name}")
+          animal_response = AnimalGateway.fetch_animal_data(predator_name)
+          
+          photo_response = AnimalGateway.fetch_photo_data(predator_name)
 
-          if animal_response.success? 
-            photo_data = JSON.parse(photo_response.body, symbolize_names: true)
-            animal_data = JSON.parse(animal_response.body, symbolize_names: true)
-            selected_animal = animal_data.find do |animal|
-              animal[:locations].include?("North-America") &&
-              animal.dig(:characteristics, :habitat) &&
-              animal.dig(:characteristics, :predators) &&
-              ( animal.dig(:characteristics, :prey) ||
-              animal.dig(:characteristics, :main_prey) )
+              AnimalDetail.new(animal_response, photo_response) if animal_response
             end
-
-            if selected_animal
-             predator = Animal.create(
-                name: selected_animal[:name],
-                photo_url: photo_data[:photos].first[:url],
-                prey: (selected_animal.dig(:characteristics, :main_prey) || selected_animal.dig(:characteristics, :prey)),
-                predators: selected_animal.dig(:characteristics, :predators),
-                habitat: selected_animal.dig(:characteristics, :habitat),
-                scientific_name: selected_animal.dig(:taxonomy, :scientific_name)
-                )
-              predator
-            end
-          else
-            Rails.logger.error("Failed to fetch data for #{predator_name}")
-          end
-        end
-        return render json: predators_data
+         render json: rabbit
       else
         render json: { error: "Rabbit not found" }, status: :not_found
       end
-    else
-      render json: { error: "Invalid action type" }, status: :bad_request
     end
   end
 
