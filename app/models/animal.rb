7a78e9c6ecfb97d131b9aefddc00efde15ae87c0
Candice Cirbo <thecirbos@icloud.com>
2024-11-02 21:01:57 -0600

    Feat: adds methods to model to slim down controller

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -14,4 +14,28 @@ class Animal < ApplicationRecord
             :weight,
             :fun_fact,
             presence: true
+
+    # def predators_with_data
+    #   predators = self.predators.split(',').map(&:strip).map(&:singularize).first(3)
+    #   predators_data = predators.map do |predator_name|
+    #     Animal.where("name ILIKE '%#{predator_name}%'")
+    #   end.flatten
+    # end
+
+    def predators_with_data
+      predators = self.predators.split(',').map(&:strip).map(&:singularize).first(3)
+      predators_data = predators.flat_map do |predator_name|
+        Animal.where("name ILIKE ?", "%#{predator_name}%").limit(1) 
+      end
+      
+    end
+
+    def create_predators_data
+      predators_with_data.each do |predator|
+        animal_response = AnimalGateway.fetch_animal_data(predator.name)
+        photo_response = AnimalGateway.fetch_photo_data(predator.name)
+        new_animal = AnimalDetail.new(animal_response, photo_response).as_json if animal_response
+        Animal.create(new_animal)
+      end
+    end
 end
