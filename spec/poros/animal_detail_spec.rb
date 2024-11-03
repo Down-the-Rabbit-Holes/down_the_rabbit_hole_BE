840f6ac317754c22154f923a58077d420d398744
Jim Macur <jim.macur@gmail.com>
2024-11-03 16:36:50 -0700

    test: adds test for correct animal info in animal detail PORO and 4 fixtures

---
+++ spec/poros/animal_detail_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe AnimalDetail do
+  describe "initialize" do
+    let(:animal_data) { JSON.parse(File.read('spec/fixtures/animal_fixtures/rabbit_animal_data.json'), symbolize_names: true) }
+    let(:photo_data) { JSON.parse(File.read('spec/fixtures/animal_fixtures/rabbit_photo_data.json'), symbolize_names: true) }
+    let(:animal) { AnimalDetail.new(animal_data, photo_data) }
+
+    it 'initializes with name' do
+      expect(animal.name).to eq("Rabbit")
+    end
+
+    it 'initializes with photo_url' do
+      expect(animal.photo_url).to eq("www.pexels.com/photo/close-up-of-rabbit-on-field-247373/")
+    end
+
+    it 'initializes with characteristics' do
+      expect(animal.prey).to eq("Clover, Grass, Crunchy vegetables")
+      expect(animal.predators).to eq("Fox, wolf, owl")
+      expect(animal.habitat).to eq("forest thickets, meadows and woodland")
+      expect(animal.fun_fact).to eq("There are more than 50 different species!")
+      expect(animal.top_speed).to eq("18 miles per hour")
+      expect(animal.life_span).to eq("4-9 years")
+      expect(animal.weight).to eq("0.5-3kg (1.1-6.6lbs)")
+      expect(animal.diet).to eq("Herbivore")
+      expect(animal.scientific_name).to eq("Oryctolagus cuniculus")
+    end
+  end
+end
