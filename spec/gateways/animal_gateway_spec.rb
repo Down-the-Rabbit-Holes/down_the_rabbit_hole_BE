fc1522e5b8b783f9ecd43e3c82cc9678f2f8eccd
Stefan Bloom <stefanjbloom88@gmail.com>
2024-11-03 20:09:04 -0700

    test: 3 testable methods in gateway testing green.

--- spec/gateways/animal_gateway_spec.rb
+++ spec/gateways/animal_gateway_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe AnimalGateway do
   describe 'happy path' do
-    it '#fetch_animal_data for animal facts', :vcr do
+    it '#fetch_animal_data fetches animal facts', :vcr do
       animal_data = AnimalGateway.fetch_animal_data("Rabbit")
 
       expect(animal_data).to be_a Hash
@@ -18,8 +18,59 @@ RSpec.describe AnimalGateway do
       expect(animal_data[:taxonomy][:scientific_name]).to eq("Oryctolagus cuniculus")
     end
 
-    it '#fetch_animal_data for animal photo', :vcr do
+    it '#fetch_animal_data fetches animal photo', :vcr do
+      animal_pic = AnimalGateway.fetch_photo_data("Rabbit")
 
+      expect(animal_pic).to be_a Hash
+      expect(animal_pic[:photos]).to be_a Array
+      expect(animal_pic[:photos].first[:url]).to include("https://www.pexels.com/photo/")
+      expect(animal_pic[:photos].first[:src][:original]).to include("https://images.pexels.com/photos/")
+      expect(animal_pic[:photos].last[:url]).to include("https://www.pexels.com/photo/")
+      expect(animal_pic[:photos].last[:src][:original]).to include("https://images.pexels.com/photos/")
+    end
+
+    let(:animals) do
+      [
+        {
+          name: "Rabbit",
+          locations: ["test", "North-America"],
+          characteristics: {
+            habitat: "my front lawn",
+            predators: ["corgis", "Humans"],
+            prey: "grass",
+            diet: "herbivore",
+            top_speed: "10 mph",
+            lifespan: "2-4 years",
+            weight: "2 pounds",
+            slogan: "test"
+          }
+        },
+        {
+          name: "Corgi",
+          locations: ["Europe"],
+          characteristics: {
+            habitat: "behind you!!!!",
+            predators: ["APEX PREDATOR"],
+            prey: "my heart",
+            diet: "nomnivore",
+            top_speed: "42 mph",
+            lifespan: "infinity :')",
+            weight: "25 pounds",
+            slogan: "test"
+          }
+        }
+      ]
+    end
+    
+    it '#valid_animal? checks if an animal object response has correct PORO attributes' do
+      valid_animal = AnimalGateway.valid_animal?(animals)
+
+      expect(valid_animal).to be_a Hash
+      expect(valid_animal[:name]).to eq("Rabbit")
+      expect(valid_animal[:name]).not_to eq("Corgi")
+      expect(valid_animal[:locations]).to include("North-America")
+      expect(valid_animal.dig(:characteristics, :habitat)).to eq("my front lawn")
+      expect(valid_animal.dig(:characteristics, :slogan)).to eq("test")
     end
   end
 end
