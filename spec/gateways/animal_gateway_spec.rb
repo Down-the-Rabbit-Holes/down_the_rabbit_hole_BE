add4bec78b3be169c147acd171ac0007d3fe868a
Stefan Bloom <stefanjbloom88@gmail.com>
2024-11-03 19:32:12 -0700

    test:#fetch_animal_data testing green

--- spec/gateways/animal_gateway_spec.rb
+++ spec/gateways/animal_gateway_spec.rb
@@ -3,20 +3,19 @@ require 'rails_helper'
 RSpec.describe AnimalGateway do
   describe 'happy path' do
     it '#fetch_animal_data for animal facts', :vcr do
-      animal_data = AnimalGateway.fetch_animal_data("rabbit")
+      animal_data = AnimalGateway.fetch_animal_data("Rabbit")
 
       expect(animal_data).to be_a Hash
-      expect(animal_data[:name]).to eq("Jackrabbit")
-      # expect(animal_data[:photo_url]).to eq(nil)
-      expect(animal_data[:characteristics][:prey]).to eq("Grass, twigs, bark")
-      expect(animal_data[:characteristics][:predators]).to eq("coyotes, red-tailed hawks, bobcats")
-      expect(animal_data[:characteristics][:habitat]).to eq("Desert, savanna, grassland")
+      expect(animal_data[:name]).to eq("Rabbit")
+      expect(animal_data[:characteristics][:main_prey]).to eq("Clover, Grass, Crunchy vegetables")
+      expect(animal_data[:characteristics][:predators]).to eq("Foxes, wolves, bobcats, eagles, owls, coyotes")
+      expect(animal_data[:characteristics][:habitat]).to eq("Forest thickets, meadows and woodland")
       expect(animal_data[:characteristics][:slogan]).to eq("There are more than 50 different species!")
-      expect(animal_data[:characteristics][:top_speed]).to eq("18 miles per hour")
-      expect(animal_data[:characteristics][:life_span]).to eq("4-9 years")
+      expect(animal_data[:characteristics][:top_speed]).to eq("2.4 mph")
+      expect(animal_data[:characteristics][:lifespan]).to eq("4-9 years")
       expect(animal_data[:characteristics][:weight]).to eq("0.5-3kg (1.1-6.6lbs)")
       expect(animal_data[:characteristics][:diet]).to eq("Herbivore")
-      expect(animal_data[:characteristics][:scientific_name]).to eq("oryctolagus cuniculus")
+      expect(animal_data[:taxonomy][:scientific_name]).to eq("Oryctolagus cuniculus")
     end
 
     it '#fetch_animal_data for animal photo', :vcr do
