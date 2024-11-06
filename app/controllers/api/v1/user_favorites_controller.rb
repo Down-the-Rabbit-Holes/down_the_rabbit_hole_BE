d37e1efbe624c27bdec4c39efe02b2f78e97b8ad
Jim Macur <jim.macur@gmail.com>
2024-11-05 20:38:18 -0700

    refactor: adds helper methods and before action in theuser favorite controller

--- app/controllers/api/v1/user_favorites_controller.rb
+++ app/controllers/api/v1/user_favorites_controller.rb
@@ -1,36 +1,30 @@
 require 'active_support/inflector'
 
 class Api::V1::UserFavoritesController < ApplicationController
+  before_action :set_user
+  before_action :set_animal, only: [:create, :destroy]
 
   def index
-    user = User.find(params[:user_id])
-    favorited_animals = user.animals
+    favorited_animals = @user.animals
     if favorited_animals.empty?
       render json: { error: "No favorites yet" }, status: :not_found
     else
       render json: favorited_animals, status: :ok
     end
-    
   end
 
   def create
-    user = User.find(params[:user_id])
-    favorite_animal = Animal.find(params[:animal_id])
-    
-    user_favorite = UserFavorite.new(user_id: user.id, animal_id: favorite_animal.id)
+    user_favorite = UserFavorite.new(user_id: @user.id, animal_id: @animal.id)
     
     if user_favorite.save
-      render json: favorite_animal, status: :created
+      render json: @animal, status: :created
     else
       render json: { error: "Unable to save favorite" }, status: :unprocessable_entity
     end
   end
 
   def destroy
-    user = User.find(params[:user_id])
-    favorite_animal = Animal.find(params[:animal_id])
-    
-    user_favorite = UserFavorite.find_by(user_id: user.id, animal_id: favorite_animal.id)
+    user_favorite = UserFavorite.find_by(user_id: @user.id, animal_id: @animal.id)
 
     if user_favorite
       user_favorite.destroy
@@ -39,5 +33,16 @@ class Api::V1::UserFavoritesController < ApplicationController
       render json: { error: "Favorite not found" }, status: :not_found
     end
   end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def set_animal
+    @animal = Animal.find(params[:animal_id])
+  end
+
 end
 
