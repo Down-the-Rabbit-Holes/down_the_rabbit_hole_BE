b0ce70274a256303de6691deb15c9b760af1be91
Jim Macur <jim.macur@gmail.com>
2024-11-05 19:16:01 -0700

    refactor: adds error handling to def index and minor changes to def create in user favorites controller

--- app/controllers/api/v1/user_favorites_controller.rb
+++ app/controllers/api/v1/user_favorites_controller.rb
@@ -5,15 +5,19 @@ class Api::V1::UserFavoritesController < ApplicationController
   def index
     user = User.find(params[:user_id])
     favorited_animals = user.animals
-
-    render json: favorited_animals, status: :ok
+    if favorited_animals.empty?
+      render json: { error: "No favorites yet" }, status: :not_found
+    else
+      render json: favorited_animals, status: :ok
+    end
+    
   end
 
   def create
     user = User.find(params[:user_id])
     favorite_animal = Animal.find(params[:animal_id])
     
-    user_favorite = UserFavorite.new(user_id: params[:user_id], animal_id: params[:animal_id])
+    user_favorite = UserFavorite.new(user_id: :user.id, animal_id: :favortie_animal.id)
     
     if user_favorite.save
       render json: favorite_animal, status: :created
