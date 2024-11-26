6124c5428855c1ae410732ab787f7b5d789e9cb6
Stefan Bloom <stefanjbloom88@gmail.com>
2024-11-25 19:32:58 -0700

    refactor of parsing in videogateway to return a video poro instance.  video poro fleshed out

--- app/poros/video.rb
+++ app/poros/video.rb
@@ -1,5 +1,12 @@
 class Video
 
-  
+  attr_reader :video_id, :title, :description, :thumbnail_url
+
+  def initialize(vid_data)
+    @video_id = vid_data[:id][:videoId]
+    @title = vid_data[:snippet][:title]
+    @description = vid_data[:snippet][:description]
+    @thumbnail_url = vid_data[:snippet][:thumbnails][:default][:url]
+  end
 
 end
