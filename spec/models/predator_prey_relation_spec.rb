a44e7cf512351942aad61cc8a4af97d6e8ad73a7
Renee Messersmith <morganrmessersmith@gmail.com>
2024-12-08 20:53:11 -0500

    Test: predator prey relations is at 100 coverage

--- spec/models/predator_prey_relation_spec.rb
+++ spec/models/predator_prey_relation_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe PredatorPreyRelation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:predator) }
+    it { should belong_to(:prey) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:predator_id) }
+    it { should validate_presence_of(:prey_id) }
+  end
 end
