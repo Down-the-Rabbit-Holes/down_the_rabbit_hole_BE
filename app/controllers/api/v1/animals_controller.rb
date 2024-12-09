86f1f36ad91c4f53726f681d66b3fbd723431dcb
Renee Messersmith <morganrmessersmith@gmail.com>
2024-12-08 20:21:10 -0500

    Test: user favorites controller is at 100

--- app/controllers/api/v1/animals_controller.rb
+++ app/controllers/api/v1/animals_controller.rb
@@ -8,25 +8,4 @@ class Api::V1::AnimalsController < ApplicationController
       render json: { error: "Animal not found" }, status: :not_found
     end
   end
-
-  # def relationships
-  #   animal = Animal.find_by(id: params[:id])
-  #   if animal
-  #     if filter_params[:predators]
-  #       render json: AnimalSerializer.new(animal.predators)
-  #     elsif filter_params[:prey]
-  #       render json: AnimalSerializer.new(animal.prey)
-  #     else
-  #       render json: { error: "No valid filter provided" }, status: :bad_request
-  #     end
-  #   else
-  #     render json: { error: "Animal not found" }, status: :not_found
-  #   end
-  # end
-  
-  # private
-  
-  # def filter_params
-  #   params.permit(:predators, :prey)
-  # end
 end
--- spec/requests/api/v1/user_favorites_request_spec.rb
+++ spec/requests/api/v1/user_favorites_request_spec.rb
@@ -4,19 +4,23 @@ RSpec.describe "UserFavorites", type: :request do
   describe "GET /index" do
     before(:each) do
       @user = User.create!(name: "first_user")
-      @animal = Animal.create!(
-        name: "test", 
-        photo_url: "www.pexels.com/photo/two-playful-coyotes-27067820/", 
-        prey: "Rabbit, Mice, Deer",
-        predators: "Human, Bears, Wolves, Great horned owls, Bald Eagles", 
-        habitat: "Forests, plains and deserts",
-        fun_fact: "Also known as the Prairie Wolf!",
-        top_speed: "40 miles per hour",
-        life_span: "10 - 15 years",
-        weight: "7kg - 21kg (15lbs - 46lbs)",
-        diet: "Carnivore",
-        scientific_name: "Canis latrans"
-        )
+      @rabbit = Animal.create!(
+        name: "rabbit",
+        photo_url: "http://example.com/rabbit.jpg",
+        habitat: "forest",
+        scientific_name: "Oryctolagus cuniculus",
+        fun_fact: "Rabbits have 360-degree vision.",
+        top_speed: "45 km/h",
+        life_span: "9 years",
+        weight: "2 kg",
+        diet: "herbivore",
+        description: "This is a rabbit",
+        group_name: "Group of rabbits",
+        baby_name: "bunny",
+        height: "small",
+        length: "also small",
+        animal_type: "rabbit"
+      )
     end
 
     it 'returns an error message if no favorites are found' do
@@ -28,7 +32,7 @@ RSpec.describe "UserFavorites", type: :request do
     end
     
     it 'returns a list of favorited animals' do
-      UserFavorite.create!(user_id: @user.id, animal_id: @animal.id)
+      UserFavorite.create!(user_id: @user.id, animal_id: @rabbit.id)
 
       get "/api/v1/users/#{@user.id}/user_favorites"
 
@@ -38,70 +42,70 @@ RSpec.describe "UserFavorites", type: :request do
       parsed_body = JSON.parse(response.body)
 
       expect(parsed_body).to be_an(Array)
-      expect(parsed_body.first["name"]).to eq("test")
-      expect(parsed_body.first["photo_url"]).to eq("www.pexels.com/photo/two-playful-coyotes-27067820/")
-      expect(parsed_body.first["prey"]).to eq("Rabbit, Mice, Deer")
-      expect(parsed_body.first["predators"]).to eq("Human, Bears, Wolves, Great horned owls, Bald Eagles")
-      expect(parsed_body.first["habitat"]).to eq("Forests, plains and deserts")
-      expect(parsed_body.first["fun_fact"]).to eq("Also known as the Prairie Wolf!")
-      expect(parsed_body.first["top_speed"]).to eq("40 miles per hour")
-      expect(parsed_body.first["life_span"]).to eq("10 - 15 years")
-      expect(parsed_body.first["weight"]).to eq("7kg - 21kg (15lbs - 46lbs)")
-      expect(parsed_body.first["diet"]).to eq("Carnivore")
-      expect(parsed_body.first["scientific_name"]).to eq("Canis latrans")
-      expect(parsed_body.first["id"]).to eq(@animal.id)
-  end
+      expect(parsed_body.first["name"]).to eq("rabbit")
+      expect(parsed_body.first["photo_url"]).to eq("http://example.com/rabbit.jpg")
+      expect(parsed_body.first["habitat"]).to eq("forest")
+      expect(parsed_body.first["fun_fact"]).to eq("Rabbits have 360-degree vision.")
+      expect(parsed_body.first["top_speed"]).to eq("45 km/h")
+      expect(parsed_body.first["life_span"]).to eq("9 years")
+      expect(parsed_body.first["weight"]).to eq("2 kg")
+      expect(parsed_body.first["diet"]).to eq("herbivore")
+      expect(parsed_body.first["scientific_name"]).to eq("Oryctolagus cuniculus")
+      expect(parsed_body.first["id"]).to eq(@rabbit.id)
+    end
   end
 
   describe "POST /create" do
     before(:each) do
       @user = User.create!(name: "first_user")
-      @animal = Animal.create!(
-        name: "test_1", 
-        photo_url: "www.pexels.com/photo/two-playful-coyotes-27067820/", 
-        prey: "Rabbit, Mice, Deer",
-        predators: "Human, Bears, Wolves, Great horned owls, Bald Eagles", 
-        habitat: "Forests, plains and deserts",
-        fun_fact: "Also known as the Prairie Wolf!",
-        top_speed: "40 miles per hour",
-        life_span: "10 - 15 years",
-        weight: "7kg - 21kg (15lbs - 46lbs)",
-        diet: "Carnivore",
-        scientific_name: "Canis latrans"
-        )
+      @rabbit = Animal.create!(
+        name: "rabbit",
+        photo_url: "http://example.com/rabbit.jpg",
+        habitat: "forest",
+        scientific_name: "Oryctolagus cuniculus",
+        fun_fact: "Rabbits have 360-degree vision.",
+        top_speed: "45 km/h",
+        life_span: "9 years",
+        weight: "2 kg",
+        diet: "herbivore",
+        description: "This is a rabbit",
+        group_name: "Group of rabbits",
+        baby_name: "bunny",
+        height: "small",
+        length: "also small",
+        animal_type: "rabbit"
+      )
     end
 
     it 'creates a new favorite' do
-      post "/api/v1/users/#{@user.id}/user_favorites", params: { animal_id: @animal.id }
+      post "/api/v1/users/#{@user.id}/user_favorites", params: { animal_id: @rabbit.id }
 
       expect(response).to have_http_status(:created)
 
       parsed_body = JSON.parse(response.body)
 
-      expect(parsed_body["name"]).to eq("test_1")
-      expect(parsed_body["photo_url"]).to eq("www.pexels.com/photo/two-playful-coyotes-27067820/")
-      expect(parsed_body["prey"]).to eq("Rabbit, Mice, Deer")
-      expect(parsed_body["predators"]).to eq("Human, Bears, Wolves, Great horned owls, Bald Eagles")
-      expect(parsed_body["habitat"]).to eq("Forests, plains and deserts")
-      expect(parsed_body["fun_fact"]).to eq("Also known as the Prairie Wolf!")
-      expect(parsed_body["top_speed"]).to eq("40 miles per hour")
-      expect(parsed_body["life_span"]).to eq("10 - 15 years")
-      expect(parsed_body["weight"]).to eq("7kg - 21kg (15lbs - 46lbs)")
-      expect(parsed_body["diet"]).to eq("Carnivore")
-      expect(parsed_body["scientific_name"]).to eq("Canis latrans")
-      expect(parsed_body["id"]).to eq(@animal.id)
+      expect(parsed_body["name"]).to eq("rabbit")
+      expect(parsed_body["photo_url"]).to eq("http://example.com/rabbit.jpg")
+      expect(parsed_body["habitat"]).to eq("forest")
+      expect(parsed_body["fun_fact"]).to eq("Rabbits have 360-degree vision.")
+      expect(parsed_body["top_speed"]).to eq("45 km/h")
+      expect(parsed_body["life_span"]).to eq("9 years")
+      expect(parsed_body["weight"]).to eq("2 kg")
+      expect(parsed_body["diet"]).to eq("herbivore")
+      expect(parsed_body["scientific_name"]).to eq("Oryctolagus cuniculus")
+      expect(parsed_body["id"]).to eq(@rabbit.id)
 
-      user_favorite = UserFavorite.find_by(user_id: @user.id, animal_id: @animal.id)
+      user_favorite = UserFavorite.find_by(user_id: @user.id, animal_id: @rabbit.id)
       
       expect(user_favorite).to be_a(UserFavorite)
       expect(user_favorite.user_id).to eq(@user.id)
-      expect(user_favorite.animal_id).to eq(@animal.id)
+      expect(user_favorite.animal_id).to eq(@rabbit.id)
     end
 
     it 'returns an error message if favorite cannot be saved' do
-      UserFavorite.create!(user_id: @user.id, animal_id: @animal.id)
+      UserFavorite.create!(user_id: @user.id, animal_id: @rabbit.id)
 
-      post "/api/v1/users/#{@user.id}/user_favorites", params: { animal_id: @animal.id }
+      post "/api/v1/users/#{@user.id}/user_favorites", params: { animal_id: @rabbit.id }
 
       expect(response).to have_http_status(:unprocessable_entity)
 
@@ -115,17 +119,21 @@ RSpec.describe "UserFavorites", type: :request do
     before(:each) do
       @user = User.create!(name: "first_user")
       @animal = Animal.create!(
-        name: "test_2", 
-        photo_url: "www.pexels.com/photo/two-playful-coyotes-27067820/", 
-        prey: "Rabbit, Mice, Deer",
-        predators: "Human, Bears, Wolves, Great horned owls, Bald Eagles", 
-        habitat: "Forests, plains and deserts",
-        fun_fact: "Also known as the Prairie Wolf!",
-        top_speed: "40 miles per hour",
-        life_span: "10 - 15 years",
-        weight: "7kg - 21kg (15lbs - 46lbs)",
-        diet: "Carnivore",
-        scientific_name: "Canis latrans"
+        name: "rabbit",
+        photo_url: "http://example.com/rabbit.jpg",
+        habitat: "forest",
+        scientific_name: "Oryctolagus cuniculus",
+        fun_fact: "Rabbits have 360-degree vision.",
+        top_speed: "45 km/h",
+        life_span: "9 years",
+        weight: "2 kg",
+        diet: "herbivore",
+        description: "This is a rabbit",
+        group_name: "Group of rabbits",
+        baby_name: "bunny",
+        height: "small",
+        length: "also small",
+        animal_type: "rabbit"
         )
 
       @user_favorite = UserFavorite.create!(user_id: @user.id, animal_id: @animal.id)
